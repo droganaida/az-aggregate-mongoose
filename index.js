590bc3b24469ca35e97b96bbf5fc465fba9f6be7
Aida <aida.drogan.box@gmail.com>
2018-01-23 09:56:20 +0300

    mongodb aggregation

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-
 const http = require('http');
 const fs = require('fs');
 const port = 6009;
 const ejs = require('ejs');
 const Item = require('./models/items').Items;
 const {promisify} = require('util');
+const shopCard = require('./libs/shopCard');
 
 const requestHandler = (request, response) => {
 
     let params = {};
     params.copyright = '#BlondieCode © ' + (new Date()).getFullYear();
 
-    const wordArray = ['Молоко', 'Бананы', 'Яблоко', 'Курица', 'Пиво', 'Огурцы', 'Апельсин'];
+    const wordArray = shopCard.itemsRu;
+
+    const renderFile = promisify(ejs.renderFile).bind(ejs);
 
     if (request.url.indexOf('.') != -1) {
 
@@ -25,7 +27,9 @@ const requestHandler = (request, response) => {
                 console.error(`Ошибка сервера ${err}`);
             });
 
-    } else {
+    } else if (request.url == '/insert') {
+
+        params.page = 'insert';
 
         //=========== async\await запускаем массив функций параллельно ============//
 
@@ -39,7 +43,19 @@ const requestHandler = (request, response) => {
             });
 
             await Promise.all(reviewPromises);
-            return await Item.find().sort({title: 1});
+
+            //=========== aggregate для сортировки записей без учета регистра (ASCII) ============//
+
+            return await Item.aggregate([
+                {$project:
+                    {
+                        _id: 0,
+                        title: 1,
+                        sortTitle: {"$toLower": "$title"}
+                    }
+                },
+                {$sort: { "sortTitle": 1 } }
+            ]);
         }
 
         //=========== асинхронный цикл for ============//
@@ -65,8 +81,6 @@ const requestHandler = (request, response) => {
             return await Item.find().sort({title: 1});
         }
 
-        const renderFile = promisify(ejs.renderFile).bind(ejs);
-
         async function renderTemplate() {
 
             params.items = await parallelMongoQuery();
@@ -74,6 +88,94 @@ const requestHandler = (request, response) => {
         }
 
         renderTemplate()
+            .then((html) => {
+                response.end(html);
+            })
+            .catch((err) => {
+                console.error(err.stack);
+                response.end('=(');
+            });
+    } else {
+
+        params.page = 'aggregate';
+
+        async function azAggregate() {
+
+            //=========== aggregate для группировки записей по первой букве ============//
+
+            const itemList = await Item.aggregate([
+                { $match: {}},
+                {$project:
+                    {
+                        _id: 0,
+                        title: 1,
+                        letter: {
+                            $let: {
+                                vars: {
+                                    numArray: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+                                    lowerFirstLetter: {$substrCP: [{ $toLower: "$title" }, 0, 1]}
+                                },
+                                in: {
+                                    $cond: {
+                                        if: { $in: [ "$$lowerFirstLetter", "$$numArray" ] },
+                                        then: '0-9',
+                                        else: "$$lowerFirstLetter"
+                                    }
+                                }
+                            }
+                        }
+                    }
+                },
+                {$group:
+                    {
+                        _id: "$letter",
+                        items: {
+                            $push: {
+                                title: "$title"
+                            }
+                        },
+                        count: {$sum: 1}
+                    }
+                },
+                {$sort: { "_id": 1 } }
+            ]);
+
+            //=========== Постобработка результатов выборки для группировки UTF-8 ============//
+
+            let resItems = [];
+            let indexLetter = [];
+
+            for (let iItem of itemList) {
+
+                try {
+
+                    let position = indexLetter.indexOf(iItem._id.toLowerCase());
+
+                    if (position == -1) {
+                        indexLetter.push(iItem._id.toLowerCase());
+                        resItems.push(iItem);
+                    } else {
+                        resItems[position].items = resItems[position].items.concat(iItem.items);
+                        resItems[position].count += iItem.count;
+                    }
+
+                } catch (err) {
+                    console.error(`Ошибка сервера ${err}`);
+                }
+            }
+
+            return [resItems, indexLetter];
+        }
+
+        async function renderAggregated() {
+
+            const [resItems, indexLetter] = await azAggregate();
+            params.letters = indexLetter;
+            params.azItems = resItems;
+            return await renderFile(__dirname + '/templates/template.ejs', params);
+        }
+
+        renderAggregated()
             .then((html) => {
                 response.end(html);
             })
---
+++ libs/shopCard.js
@@ -0,0 +1,50 @@
+
+let shopCard = {};
+
+shopCard.itemsRu = [
+    "Молоко",
+    "Бананы",
+    "Яблоко",
+    "Курица",
+    "Пиво",
+    "Огурцы",
+    "Апельсин",
+    "1 литр сока",
+    "2 бутылки кефира",
+    "42 пельмешки",
+    "бутерброд с сыром",
+    "брокколи",
+    "ананас",
+    "авокадо",
+    "сосиски",
+    "Семечки",
+    "сгущенка",
+    "Докторская колбаса",
+    "кукуруза",
+    "дохлая мышь"
+];
+
+shopCard.itemsEn = [
+    "Milk",
+    "Bananas",
+    "Apples",
+    "Chicken",
+    "Beer",
+    "Cucumbers",
+    "Orange",
+    "1 liter of juice",
+    "2 bottles of kefir",
+    "42 pelmets",
+    "cheese sandwich",
+    "broccoli",
+    "pineapple",
+    "avocado",
+    "sausages",
+    "Seeds",
+    "condensed milk",
+    "Doctor's sausage",
+    "corn",
+    "dead mouse"
+];
+
+module.exports = shopCard;
